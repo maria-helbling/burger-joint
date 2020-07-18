d6dbc50f4a5bc6b1e1ec1abfad412d44765317a1
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-07-17 21:38:42 -0700

    front end functionality complete, buttons and forms working and rendering

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,27 @@
+//insode a function to make sure tha page has time to load first
 $(function(){
-    
+    //button action to devour a burger
+    $('.devour').on('click', function(event){
+        
+        let id = $(this)[0].dataset.id
+        const devourObj = {devoured: 1}
+        $.ajax('/api/change/' + id, {
+            type: 'PUT', 
+            data: devourObj
+        }).then(() => location.reload()
+        ).fail(()=>location.reload())
+    });
+
+    $('.create-form').on('submit', event => {
+        event.preventDefault();
+
+        let newBurger = {
+            burger_name : $('#bn').val().trim()
+        }
+
+        $.ajax('/api/new', {
+            type: 'POST',
+            data: newBurger
+        }).then(() => location.reload()).fail(() => location.reload())
+    })
 });
